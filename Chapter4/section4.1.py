0f48669c0590174068542d4821b51b45457adf85
Aerokura <zhangmengyingkd@163.com>
2022-03-25 21:38:06 +0800

    Update section4.1.py

--- Chapter4/section4.1.py
+++ Chapter4/section4.1.py
@@ -1,3 +1,5 @@
+# Edited by Dr.Mengying Zhang  zhangmengyingkd@163.com
+# Updated on 25/03/2022
 import numpy as np
 import matplotlib.pyplot as plt
 # 4.1.1 Example: Motion of a falling Tennis Ball
